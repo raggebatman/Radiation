fec580fa9fce095c17badcd9043ee61e32b1a53b
ragge <raggebatman@tutanota.com>
2023-09-30 12:33:05 +0200

    version command
    Introduced the version command so moderators can see what version of the plugin the server is running

---
+++ Radiation/Commands/Radiation/VersionCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CommandSystem;
+
+namespace Radiation.Commands
+{
+    public class VersionCommand : ICommand
+    {
+        public string Command { get; } = "version";
+        public string Description { get; } = "Show plugin version";
+        public string[] Aliases { get; }
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "This server is using Radiation v1.1.1";
+            return true;
+        }
+    }
+}
